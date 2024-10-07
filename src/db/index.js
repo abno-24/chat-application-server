30c73a01d3ae73ac9a66d5ed1e3bbc19c8bdc940
Abno <abhinavmaurya605@gmail.com>
2024-10-07 21:18:28 +0530

    DB connection done

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+export const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\nMongoDB connected || DB Host: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("Connection ERROR: ", error);
+    process.exit(1);
+  }
+};
